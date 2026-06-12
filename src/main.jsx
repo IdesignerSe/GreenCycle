8820e4472eb6b5aa5bdc865d0d7f5449d6ba1558
idesignerSe <hebera75@gmail.com>
2026-06-12 18:37:45 +0200

    Update Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppRouter from './router'
-import './styles/theme.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./router";
+import "./styles/theme.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <AppRouter />
-)
+);
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ProductsProvider from "./context/ProductsContext";
 
 export default function AppRouter() {
   return (
     <BrowserRouter basename="/GreenCycle/">
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </BrowserRouter>
   );
 }
