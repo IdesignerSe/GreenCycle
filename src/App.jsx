1d7487ef3207ca859cf89c751875910f8753434a
idesignerSe <hebera75@gmail.com>
2026-06-12 18:55:08 +0200

    Update App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import TakeAway from "./pages/TakeAway";
+import SecondHand from "./pages/SecondHand";
+import ProductDetails from "./pages/ProductDetails";
 
 export default function App() {
   return (
     <>
       <Navbar />
+
       <main style={{ padding: "24px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/takeaway" element={<TakeAway />} />
+          <Route path="/secondhand" element={<SecondHand />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
       </main>
+
       <Footer />
     </>
   );
