c3d52e5d99b15053f2376219ae5d18d0171f21a7
idesignerSe <hebera75@gmail.com>
2026-06-12 19:57:35 +0200

    main jsx new file

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./router";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext";
+
+import "./styles/global.css";
+import "./styles/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/GreenCycle/">
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  </BrowserRouter>
+);
