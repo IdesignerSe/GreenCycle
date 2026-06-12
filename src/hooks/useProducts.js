62de4e93df68c5faac21ff1cf113ffff7829323a
idesignerSe <hebera75@gmail.com>
2026-06-12 18:31:01 +0200

    Use Products

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import { ProductsContext } from "../context/ProductsContext";
+
+export default function useProducts() {
+  const { products, setProducts } = useContext(ProductsContext);
+
+  // Get a single product by ID
+  const getProduct = (id) => {
+    return products.find((item) => item.id === id);
+  };
+
+  // Update a product (price, free, reserved, image, etc.)
+  const updateProduct = (id, updatedFields) => {
+    setProducts((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, ...updatedFields } : item
+      )
+    );
+  };
+
+  // Add a new product (Admin)
+  const addProduct = (newItem) => {
+    setProducts((prev) => [...prev, newItem]);
+  };
+
+  // Delete a product (Admin)
+  const deleteProduct = (id) => {
+    setProducts((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  // Filter: free items only
+  const getFreeItems = () => {
+    return products.filter((item) => item.free === true);
+  };
+
+  // Filter: priced items only
+  const getPaidItems = () => {
+    return products.filter((item) => item.free === false);
+  };
+
+  return {
+    products,
+    setProducts,
+    getProduct,
+    updateProduct,
+    addProduct,
+    deleteProduct,
+    getFreeItems,
+    getPaidItems
+  };
+}
