07fba5208cae94e0254bda3461ee20d9595e2dd0
idesignerSe <hebera75@gmail.com>
2026-06-12 13:35:40 +0200

    Adding Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
-export default defineConfig({
-  plugins: [react()],
-  base: '/GreenCycle/'
-})
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: "24px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
