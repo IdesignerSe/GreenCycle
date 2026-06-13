0a2425e8a0e297f07f397e5d25a02ad806398043
idesignerSe <hebera75@gmail.com>
2026-06-13 10:31:03 +0200

    issue blanq page Update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router";
 import AuthProvider from "./context/AuthContext.jsx";
-import "./styles/global.css";
-import "./styles/theme.css";
-
+import ProductsProvider from "./context/ProductsContext.jsx";
 
 import "./styles/global.css";
 import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <ProductsProvider>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </ProductsProvider>
   </React.StrictMode>
 );
